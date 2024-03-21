d0e95e461e1ed32349a9a2accab303c2318c07c1
edgar-maddocks <edgarmaddocks@outlook.com>
2024-03-21 10:55:36 +0000

    Start Convolutional Layer

--- nn/layers.py
+++ nn/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import correlate2d, convolve
 from tensor import Tensor
 from typing import Dict, Callable
 
@@ -8,6 +9,7 @@ class Layer:
         self.params: Dict[str, Tensor] = {}
         self.grads: Dict[str, Tensor] = {}
         self.input = None
+        self.output = None
 
     def forward(self, input: Tensor) -> Tensor:
         raise NotImplementedError
@@ -24,6 +26,8 @@ class Dense(Layer):
         self.params["w"] = Tensor(np.random.randn(n_features, n_nodes))
         self.params["b"] = Tensor(np.random.randn(n_nodes))
 
+        self.layer_type = "Dense"
+
     def forward(self, input: Tensor) -> Tensor:
         self.input = input
         return Tensor(np.dot(input.data, self.params["w"].data) + self.params["b"].data)
@@ -32,6 +36,35 @@ class Dense(Layer):
         self.grads["b"] = Tensor(np.sum(output_grad.data, axis=0))
         self.grads["w"] = Tensor(np.dot(self.input.data.T, output_grad.data))
         return Tensor(np.dot(output_grad.data, self.params["w"].data.T))
+
+
+class Convolutional(Layer):
+    def __init__(
+        self, input_shape: tuple[int], kernel_size: int, n_kernels: int
+    ) -> None:
+        input_depth, input_width, input_height = input_shape
+        self.input_shape = input_shape
+        self.input_depth = input_depth
+
+        self.depth = n_kernels
+        self.output_shape = (
+            self.depth,
+            (input_height - kernel_size + 1),
+            (input_height - kernel_size + 1),
+        )
+        self.kernels_shape = (self.depth, self.input_depth, kernel_size, kernel_size)
+
+        self.params["w"] = Tensor(np.random.randn(*self.kernels_shape))
+        self.params["b"] = Tensor(np.random.randn(*self.output_shape))
+
+    def forward(self, input: Tensor) -> Tensor:
+        self.output = np.copy(self.params["b"].data)
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                self.output[i] += correlate2d(
+                    self.input[j], self.kernels[i, j], "valid"
+                )
+        return self.output
 
 
 F = Callable[[Tensor], Tensor]
